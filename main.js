eb29cc579a9e658b2846fa3788de7045a8a340c6
HHZKYO <1692189552@qq.com>
2024-10-26 17:22:10 +0800

    尝试使用了mock去get一个请求

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 import './style.css'
 import axios from 'axios'
 
-console.log(axios)
+
+
+// 向给定ID的用户发起请求
+axios.get('https://apifoxmock.com/m1/4000302-3636257-default/me')
+  .then(function (response) {
+    // 处理成功情况
+    console.log(response);
+  })
+  .catch(function (error) {
+    // 处理错误情况
+    console.log(error);
+  })
+  .finally(function () {
+    // 总是会执行
+  });
+
