cbc40a72ef0f2f261cb1e397d4fd4c70e8f2737b
HHZKYO <1692189552@qq.com>
2024-10-26 17:57:53 +0800

    form标签可提交

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 import './style.css'
 import axios from 'axios'
 
+const signInForm = document.querySelector('#signInForm')
 
-
-// 向给定ID的用户发起请求
-axios.get('https://apifoxmock.com/m1/4000302-3636257-default/me')
-  .then(function (response) {
-    // 处理成功情况
-    console.log(response);
-  })
-  .catch(function (error) {
-    // 处理错误情况
-    console.log(error);
-  })
-  .finally(function () {
-    // 总是会执行
-  });
+signInForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log('提交')
+})
 
