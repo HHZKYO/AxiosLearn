66e0707238101a7422d89ccdc8a81ebe6b5bd083
HHZKYO <1692189552@qq.com>
2024-10-26 18:41:50 +0800

    mock了一个发送验证码的请求

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
 import axios from 'axios'
 
+axios.defaults.baseURL = 'https://apifoxmock.com/m1/4000302-3636257-default'
+
 const signInForm = document.querySelector('#signInForm')
 
 signInForm.addEventListener('submit', (e) => {
@@ -8,3 +10,15 @@ signInForm.addEventListener('submit', (e) => {
   console.log('提交')
 })
 
+const sendCodeBtn = document.querySelector('#sendCodeBtn')
+sendCodeBtn.addEventListener('click', (e) => {
+  const emailInput = document.querySelector('#emailInput')
+  if(emailInput === undefined){ return }
+  const email = emailInput.value
+  axios.post('/validation_codes', { email })
+    .then(() => {
+      window.alert('发送成功')
+    }, () => {
+      window.alert('发送失败')
+    })
+})
